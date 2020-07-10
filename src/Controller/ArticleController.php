9c62bea9cde8155c30deaeecc350c60c6782ae4a
Philemon GLOBLEHI <philemongloblehi@gmail.com>
2020-07-10 17:09:44 +0000

    Use FOSRestBundle

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,55 +3,65 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use JMS\Serializer\SerializationContext;
+use App\Form\ArticleType;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class ArticleController extends AbstractController
+class ArticleController extends AbstractFOSRestController
 {
     /**
-     * @Route("/articles/{id}", name="article_show", methods={"GET"})
+     * @Rest\Get(
+     *     path = "/articles/{id}",
+     *     name = "app_article_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View()
      * @param Article $article
-     * @return Response
+     * @return Article
      */
     public function showAction(Article $article) {
-
-        $data = $this->get('jms_serializer')->serialize($article, 'json', SerializationContext::create()->setGroups(array('details')));
-
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
-    }
+       return $article;
+   }
 
     /**
-     * @Route("/articles", name="article_create", methods={"POST"})
+     * @Rest\Post(
+     *     path = "/articles",
+     *     name = "app_article_create"
+     * )
+     * @Rest\View(StatusCode = 201)
+     * @ParamConverter("article", converter="fos_rest.request_body")
      * @param Request $request
-     * @return Response
+     * @return View
      */
-    public function createAction(Request $request) {
-        $data = $request->getContent();
-        $article = $this->get('jms_serializer')->deserialize($data, Article::class, 'json');
+   public function createAction(Request $request) {
+       $data = $this->get('jms_serializer')->deserialize($request->getContent(), 'array', 'json');
+       $article = new Article();
+       $form = $this->get('form.factory')->create(ArticleType::class, $article);
+       $form->submit($data);
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($article);
         $em->flush();
 
-        return new Response('', Response::HTTP_CREATED);
+        return $this->view($article, Response::HTTP_CREATED,
+            [
+                'Location' => $this->generateUrl('app_article_show',
+                    [
+                        'id' => $article->getId(), UrlGeneratorInterface::ABSOLUTE_URL
+                    ])
+            ]);
     }
 
     /**
-     * @Route("/articles", name="article_list", methods={"GET"})
+     * @Rest\Get("/articles", name="app_article_list")
      */
     public function listAction() {
-        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
-        $data = $this->get('jms_serializer')->serialize($articles, 'json', SerializationContext::create()->setGroups(array('list')));
-
-        $response = new Response();
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
+        return $this->getDoctrine()->getRepository(Article::class)->findAll();
     }
 }
